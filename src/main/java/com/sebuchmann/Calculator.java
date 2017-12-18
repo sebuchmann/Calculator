92cc2596e508a8b6abd6a28cbe7e37e71caf945b
5ia15sebuchmann <5ia15sebuchmann@gibwintra.ch>
2017-12-18 09:10:11 +0100

    addition added to Calculator.java

--- src/main/java/com/sebuchmann/Calculator.java
+++ src/main/java/com/sebuchmann/Calculator.java
@@ -2,5 +2,13 @@ package com.sebuchmann;
 
 public class Calculator {
 	
+	private int result;
+	
+	public int addition(int summand1, int summand2) {
+		
+		result = summand1 + summand2;
+		
+		return result;
+	}
 
 }
