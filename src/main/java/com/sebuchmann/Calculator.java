c113492da323e1be3292294560cb2e774598fcc6
5ia15sebuchmann <5ia15sebuchmann@gibwintra.ch>
2017-12-18 09:04:06 +0100

    First Project Commit

--- src/main/java/com/sebuchmann/Calculator.java
+++ src/main/java/com/sebuchmann/Calculator.java
@@ -2,13 +2,5 @@ package com.sebuchmann;
 
 public class Calculator {
 	
-	private int result;
-	
-	public int addition(int summand1, int summand2) {
-		
-		result = summand1 + summand2;
-		
-		return result;
-	}
 
 }
--- src/test/java/com/sebuchmann/CalculatorTest.java
+++ src/test/java/com/sebuchmann/CalculatorTest.java
@@ -6,13 +6,6 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	
-	@Test
-	public void testSummeZweiPositive() {
-		Calculator calc = new Calculator();
-		int result = calc.addition(1, 2);
-		assertEquals(3, result);
-	}
-	
 	
 
 }
