7491ec6136dd25e17612d483ed759da80a28bd49
5ia15sebuchmann <5ia15sebuchmann@gibwintra.ch>
2018-01-08 08:09:16 +0100

    Added Test Summe 2 Negative

--- src/test/java/com/sebuchmann/CalculatorTest.java
+++ src/test/java/com/sebuchmann/CalculatorTest.java
@@ -6,6 +6,19 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	
+	@Test
+	public void testSummeZweiPositive() {
+		Calculator calc = new Calculator();
+		int result = calc.addition(1, 2);
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void testSummeZweiNegative() {
+		Calculator calc = new Calculator();
+		int result = calc.addition(-1, -2);
+		assertEquals(-3, result);
+	}
 	
 
 }
